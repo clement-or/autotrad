817ef6c61fe94fedaf57e069f6dfabc1f2805348
Topy <topy72.mine@gmail.com>
2023-08-12 12:03:29 +0200

    Set selection window to be maximized

--- src/app.rs
+++ src/app.rs
@@ -5,60 +5,27 @@ use egui::Ui;
 use egui::{emath, CentralPanel};
 use egui::{Color32, Pos2, Rect, Rounding, Sense, Stroke};
 
+const DATA_SELECTION_ID: &str = "region_selection";
+
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum Event {
     Nothing,
-    RegionSelectionUpdated { selection: egui::Rect },
+    RegionSelectionUpdated,
     RegionSelectionFinished,
     SelectRegionButtonClicked,
 }
 
-pub struct App {
-    prev_state: State,
-    cur_state: State,
-    cur_event: Event,
-    prev_event: Event,
-
-    selection: Rect,
+#[derive(PartialEq, Debug, Clone, Copy)]
+enum State {
+    None,
+    Default,
+    SelectRegion,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            cur_state: State::Default,
-            prev_state: State::None,
-            cur_event: Event::Nothing,
-            prev_event: Event::Nothing,
-
-            selection: egui::Rect::NOTHING,
-        }
-    }
-}
-
-#[allow(unused_must_use)]
-impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        println!("State: {:?}, Event: {:?}", self.cur_state, self.cur_event);
-
-        self.cur_event = self.cur_state.run(ctx, _frame);
-        let new_state = self.get_next_state();
-
-        if self.cur_state != new_state {
-            self.cur_state.exit(ctx, _frame);
-            new_state.enter(ctx, _frame);
-
-            self.prev_state = self.cur_state;
-            self.cur_state = new_state;
-        }
-
-        self.prev_event = self.cur_event;
-    }
-
-    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
-        [0., 0., 0., 0.5]
-    }
-}
-
+//
+// --- Statemachine transition logic
+// This part handles the transitions between different states based on events received from them.
+//
 impl App {
     fn get_next_state(&self) -> State {
         match (self.cur_state, self.cur_event) {
@@ -66,21 +33,10 @@ impl App {
             (State::Default, Event::SelectRegionButtonClicked) => State::SelectRegion,
             (_, Event::Nothing) => self.cur_state,
 
-            _ => self.cur_state,
             (State::None, _) => State::Default,
+            _ => self.cur_state,
         }
     }
-}
-
-//
-// --- App state
-//
-
-#[derive(PartialEq, Debug, Clone, Copy)]
-enum State {
-    None,
-    Default,
-    SelectRegion,
 }
 
 // --- Main StateMachine logic
@@ -110,16 +66,19 @@ impl State {
     }
 }
 
+//
 // --- Default ui state
 // Default window displayed on starting the app
+//
 impl State {
     fn run_default(ctx: &egui::Context, _frame: &mut eframe::Frame) -> Event {
+        _frame.set_maximized(false);
+        _frame.set_decorations(true);
         egui::CentralPanel::default()
             .show(ctx, |ui| {
                 ui.add(egui::Label::new("Hello World!"));
                 ui.label("A shorter and more convenient way to add a label.");
                 if ui.button("Click me").clicked() {
-                    println!("Click");
                     Event::SelectRegionButtonClicked
                 } else {
                     Event::Nothing
@@ -128,21 +87,26 @@ impl State {
             .inner
     }
     fn enter_default(_ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        _frame.set_maximized(false);
-        _frame.set_window_size(egui::Vec2 { x: 500.0, y: 300.0 });
-        _frame.set_decorations(true);
+        _frame.set_window_size(egui::Vec2 { x: 200.0, y: 150.0 });
+        _frame.set_centered();
     }
     fn exit_default(_ctx: &egui::Context, _frame: &mut eframe::Frame) {}
 }
 
+//
 // --- Select region state
-// We're waiting for the player to select a region with the mouse
+// In this state, we prompt the user to select a screen region and draw a rectangle defining its
+// box selection. Player exits the state when releasing, which validates the selection and shall
+// return to default
+//
 #[allow(unused_must_use)]
 impl State {
-    fn enter_selectregion(ctx: &egui::Context, _frame: &mut eframe::Frame) {}
-    fn exit_selectregion(ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+    fn enter_selectregion(_ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+    fn exit_selectregion(_ctx: &egui::Context, _frame: &mut eframe::Frame) {}
 
     fn run_selectregion(ctx: &egui::Context, _frame: &mut eframe::Frame) -> Event {
+        _frame.set_maximized(true);
+        _frame.set_decorations(false);
         let frame = egui::Frame::default();
         {
             let mut this = frame;
@@ -154,10 +118,12 @@ impl State {
         let mut panel = CentralPanel::default();
         panel = panel.frame(frame);
 
-        panel.show(ctx, State::on_panel_shown_selectregion).inner
+        panel
+            .show(ctx, |ui| State::on_panel_shown_selectregion(ui, ctx))
+            .inner
     }
 
-    fn on_panel_shown_selectregion(ui: &mut Ui) -> Event {
+    fn on_panel_shown_selectregion(ui: &mut Ui, ctx: &egui::Context) -> Event {
         let (res, painter) = ui.allocate_painter(ui.available_size(), Sense::click_and_drag());
 
         let to_screen = emath::RectTransform::from_to(
@@ -185,6 +151,10 @@ impl State {
                 None
             }
         } {
+            ctx.data_mut(|data| {
+                data.insert_persisted(egui::Id::new(DATA_SELECTION_ID), selection);
+            });
+
             let shape = RectShape {
                 rect: selection,
                 rounding: Rounding::default(),
@@ -197,7 +167,7 @@ impl State {
 
             painter.add(shape);
 
-            return Event::RegionSelectionUpdated { selection };
+            return Event::RegionSelectionUpdated;
         }
 
         if res.drag_released_by(egui::PointerButton::Primary) {
@@ -205,5 +175,59 @@ impl State {
         }
 
         Event::Nothing
+    }
+}
+
+//
+// --- The main App acts as a state machine that defers display code to several states
+// --- States are analogous to views
+//
+
+pub struct App {
+    prev_state: State,
+    cur_state: State,
+    cur_event: Event,
+    prev_event: Event,
+
+    selection: Rect,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            cur_state: State::Default,
+            prev_state: State::None,
+            cur_event: Event::Nothing,
+            prev_event: Event::Nothing,
+
+            selection: egui::Rect::NOTHING,
+        }
+    }
+}
+
+#[allow(unused_must_use)]
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        println!("Update state: {:?}", self.cur_state);
+
+        // Handle state machine logic
+        self.cur_event = self.cur_state.run(ctx, _frame);
+        let new_state = self.get_next_state();
+
+        if self.cur_state != new_state {
+            self.cur_state.exit(ctx, _frame);
+            new_state.enter(ctx, _frame);
+
+            println!("Exit: {:?}, Enter: {:?}", self.cur_state, new_state);
+
+            self.prev_state = self.cur_state;
+            self.cur_state = new_state;
+        }
+
+        self.prev_event = self.cur_event;
+    }
+
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        [0., 0., 0., 0.5]
     }
 }
